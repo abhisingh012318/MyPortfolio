68e1873722b8f809d9311fc2a839342072f2f6ac
abhisingh012318 <abhisheksharma012318@gmail.com>
2026-01-30 22:45:48 +0530

    lenis added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from './pages/Home';
+import Lenis from "@studio-freight/lenis";
 
 function App() {
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+      smoothTouch: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <p class="text-red-500 text-center">Red text   this is  homepage s</p>
-<p class="text-blue-600">Blue text</p>
+<p class="text-blue-600 bg-amber-500 h-full p">Blue text</p>
 <p class="text-gray-700">Gray text</p>
 <p class="text-white">White text</p>
 
